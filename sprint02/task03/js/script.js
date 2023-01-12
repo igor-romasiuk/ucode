e56f5d0eafe0dcfc6dac1dc28e27d008462ee5ad
Igor <igor132143@gmail.com>
2023-01-12 15:53:22 +0200

    added changes

--- sprint02/task03/js/script.js
+++ sprint02/task03/js/script.js
@@ -1,35 +1,6 @@
-let idiom = +prompt('What kind of Number?','1')
+"use strict"
 
-switch (idiom) { 
-    case 1:
-        alert('Back to square 1');
-        break;
-    case 2:
-        alert('Goody 2-shoes');
-        break;
-    case 3:
-    case 6:    
-        alert('Two\'s company, three\'s a crowd');
-        break; 
-    case 4:
-    case 9:    
-        alert('Counting sheep');
-        break;     
-    case 5:
-        alert('Take ﬁve');
-        break; 
-    case 7:
-        alert('Seventh heaven');
-        break;     
-    case 8:
-        alert('Behind the eight-ball');
-        break;    
-    case 10:
-        alert('Cheaper by the dozen');  
-        break;
-    default:
-        num = +prompt('What kind of Number?');    
-}
+let num = +prompt('What kind of Number?','1')
 
 switch (num) { 
     case 1:
@@ -59,5 +30,35 @@ switch (num) {
         alert('Cheaper by the dozen');  
         break;
     default:
+      let numTwo = +prompt('What kind of Number?');    
+switch (numTwo) { 
+    case 1:
+        alert('Back to square 1');
+        break;
+    case 2:
+        alert('Goody 2-shoes');
+        break;
+    case 3:
+    case 6:    
+        alert('Two\'s company, three\'s a crowd');
+        break; 
+    case 4:
+    case 9:    
+        alert('Counting sheep');
+        break;     
+    case 5:
+        alert('Take ﬁve');
+        break; 
+    case 7:
+        alert('Seventh heaven');
+        break;     
+    case 8:
+        alert('Behind the eight-ball');
+        break;    
+    case 10:
+        alert('Cheaper by the dozen');  
+        break;
+    default:
         alert('Your number wrong');  
+}
 }
