b689453343e7787f53557516de70811822fcb0c9
Igor <igor132143@gmail.com>
2023-01-26 18:06:42 +0200

    completed task06

--- sprint02/task06/js/script.js
+++ sprint02/task06/js/script.js
@@ -1,33 +1,26 @@
 "use strict"
 
 
-const userName = prompt('What is your name?', 'Igor')
-const lastName = prompt('What is your last name?', 'Romasiuk')
-let fullName = `${userName} ${lastName}`
+const userName = prompt('What is your name?', 'Igor');
+const lastName = prompt('What is your last name?', 'Romasiuk');
+let fullName = `${userName} ${lastName}`;
 
-function info (fullName) {
+function greeting (fullName) {
+const reg = /^[A-Za-z ]+$/.test(fullName);
 
-let reg = /^[A-Za-z ,]+$/.test(fullName)
+if (reg) {
+  const arr = fullName.split(" ");
 
-if (reg == true) {
-const arr = fullName.split(" ");
 for (let i = 0; i < arr.length; i++) {
   arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
 }
-const str2 = arr.join(" ");
-console.log('Welcome ' + str2);
-alert('Welcome ' + str2)
+  const str2 = arr.join(" ");
+
+  console.log('Welcome ' + str2);
+  alert('Welcome ' + str2);
 } else {
-  alert('Error')
-  console.log('Error');
+  alert('Wrong input');
+  console.log('Wrong input');
 }
 }
-info(fullName)
-
-
-
-//split the above string into an array of strings 
-//whenever a blank space is encountered
-//loop through each element of the array and capitalize the first letter.
-//Join all the elements of the array back into a string 
-//using a blankspace as a separator
+greeting(fullName);
