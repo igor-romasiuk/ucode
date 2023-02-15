df0180ab54c461244eb2a0bd944b77da45a18c5e
Igor <igor132143@gmail.com>
2023-02-15 17:55:14 +0200

    change spaces and tabs

--- sprint02/task06/js/script.js
+++ sprint02/task06/js/script.js
@@ -6,21 +6,21 @@ const lastName = prompt('What is your last name?', 'Romasiuk');
 let fullName = `${userName} ${lastName}`;
 
 function greeting (fullName) {
-const reg = /^[A-Za-z ]+$/.test(fullName);
+  const reg = /^[A-Za-z ]+$/.test(fullName);
 
-if (reg) {
-  const arr = fullName.split(" ");
+  if (reg) {
+    const arr = fullName.split(" ");
 
-for (let i = 0; i < arr.length; i++) {
-  arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-}
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
   const str2 = arr.join(" ");
-
+  
   console.log('Welcome ' + str2);
   alert('Welcome ' + str2);
-} else {
-  alert('Wrong input');
-  console.log('Wrong input');
-}
+  } else {
+    alert('Wrong input');
+    console.log('Wrong input');
+  }
 }
 greeting(fullName);
