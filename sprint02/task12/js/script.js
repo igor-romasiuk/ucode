548e42a0effcd8ad1ffda98aae4f0fbab9079b22
Igor <igor132143@gmail.com>
2023-04-06 18:34:06 +0300

    corrected mistakes

--- sprint02/task12/js/script.js
+++ sprint02/task12/js/script.js
@@ -3,37 +3,63 @@
 
 
 function concat(str1, str2) {
-
-	function innerConcat(str2) {
+	function innerConcat(string) {
 		innerConcat.count++;
-		if (!str2) str2 = prompt('Enter the second string:','Hulk alone!');
-		return str1 + ' ' + str2;
+		if (!string) string = prompt('Enter the second string:','Hulk alone!');
+		return str1 + ' ' + string;
 	}
 	innerConcat.count = 0;
+  
 	if (!str2) { 
 		return innerConcat;
-	}else {
+	} else {
 		return innerConcat(str2);
 	}
 }
 
-
-
-
+/* ------- TESTING ------- */
 let phrase1 = concat('Hulk', 'smash!');
 let output = phrase1;
-console.log(output);
+console.log(output); // Hulk smash!
+
 let phrase2 = concat('Leave');
 output = phrase2();
-console.log(output);
-console.log(phrase2.count);
+// a prompt appears. Enter "Hulk alone!" into the prompt
+
+console.log(output); // Leave Hulk alone!
+console.log(phrase2.count); // 1
+
 output = phrase2();
-console.log(output);
+// a prompt appears. Enter "me alone, please!" into the prompt
+
+console.log(output); // Leave me alone, please!
+
 output = phrase2();
-console.log(output);
-console.log(phrase2.count);
+// a prompt appears. Enter "HULK ALONE!" into the prompt
+
+console.log(output); // Leave HULK ALONE!
+console.log(phrase2.count); // 3
+
 let phrase3 = concat('Go');
 output = phrase3();
-console.log(output);
-console.log(phrase3.count);
-console.log(phrase2.count);
+// a prompt appears. Enter "away!" into the prompt
+
+console.log(output); // Go away!
+console.log(phrase3.count); // 1
+console.log(phrase2.count); // 3
+
+/* Result in Console panel:
+Hulk smash!
+Leave Hulk alone!
+1
+Leave me alone, please!
+Leave HULK ALONE!
+3
+Go away!
+1
+3
+*/
+
+
+
+
